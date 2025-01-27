cb49e04df63e67375472ab56b9edd0504cd75589
dykhounphypheth charles-eric <dykhounphypheth@gmail.com>
2025-01-27 12:04:57 +0100

    exo finish

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import './App.css'
+import EmployeeCard from './components/EmployeeCard'
+const sampleEmployee = {
+  name: {
+    first: "Charlie",
+    last: "Thompson",
+  },
+  email: "charlie.thompson@example.com",
+  picture: {
+    medium: "https://randomuser.me/api/portraits/med/men/40.jpg",
+  },
+};
+
+function App() {
+  const [employee, setEmployee] = useState(sampleEmployee);
+  const getEmployee = () => {
+    // Send the request
+    fetch("https://randomuser.me/api?nat=en")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setEmployee(data.results[0]);
+      });
+  };
+
+  return (
+    <div className='App'>
+      <EmployeeCard employee={employee} />
+      <button type="button" onClick={getEmployee}>Get employee</button>
+    </div>
+    
+  );
+}
+
+export default App
